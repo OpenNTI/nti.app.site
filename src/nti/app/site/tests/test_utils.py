6924244506762989384b3aa0fdd460649db68787
Carlos Sanchez <papachoco@gmail.com>
2017-06-15 16:46:51 -0500

    add utility func to create a site

---
+++ src/nti/app/site/tests/test_utils.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import none
+from hamcrest import is_not
+from hamcrest import has_length
+from hamcrest import assert_that
+from hamcrest import has_property
+does_not = is_not
+
+from zope.component.hooks import getSite
+
+from nti.app.site.utils import create_site
+
+from nti.site.hostpolicy import get_host_site
+
+from nti.site.site import get_component_hierarchy_names
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+from nti.dataserver.tests import mock_dataserver
+
+
+class TestUtils(ApplicationLayerTest):
+
+    default_origin = 'http://janux.ou.edu'
+
+    @WithSharedApplicationMockDS(testapp=False, users=False)
+    def test_create_site(self):
+        # create root site
+        with mock_dataserver.mock_db_trans():
+            result = create_site('abydos.nextthought.com')
+            assert_that(result, is_not(none()))
+
+        # check it exists
+        with mock_dataserver.mock_db_trans():
+            result = get_host_site('abydos.nextthought.com')
+            assert_that(result, is_not(none()))
+
+        # create subsite
+        with mock_dataserver.mock_db_trans(site_name='abydos.nextthought.com'):
+            assert_that(getSite(), 
+                        has_property('__name__', is_('abydos.nextthought.com')))
+            result = create_site('seti.nextthought.com')
+            assert_that(result, is_not(none()))
+            
+        with mock_dataserver.mock_db_trans(site_name='seti.nextthought.com'):
+            names = get_component_hierarchy_names()
+            assert_that(names, has_length(2))
+            assert_that(names, is_(['seti.nextthought.com', 'abydos.nextthought.com']))
---
+++ src/nti/app/site/utils.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+
+from zope.component.hooks import getSite
+
+from zope.interface.interfaces import IComponents
+
+from zope.traversing.interfaces import IEtcNamespace
+
+from nti.appserver.policies.sites import BASEADULT
+
+from nti.site.folder import HostPolicyFolder
+from nti.site.folder import HostPolicySiteManager
+
+from nti.site.site import BTreePersistentComponents
+from nti.site.utils import registerUtility
+
+
+def create_site(name, default_parent=BASEADULT):
+    logger.info("Installing site policy %s", name)
+    hostsites = component.getUtility(IEtcNamespace, name='hostsites')
+    ds_folder = hostsites.__parent__  # by definiton
+    #  get current components
+    site_name = getattr(getSite(), '__name__', None) or ''
+    parent = component.queryUtility(IComponents, name=site_name)
+    if parent is None:
+        parent = default_parent
+        secondary_comps = ds_folder.getSiteManager()
+    else:
+        secondary_comps = component.getSiteManager()
+    # create site components
+    comps = BTreePersistentComponents(name=name, bases=(parent,))
+    comps.__parent__ = parent
+    # register components
+    registerUtility(ds_folder.getSiteManager(),
+                    comps,
+                    provided=IComponents,
+                    name=name)
+    # create policy folder
+    result = HostPolicyFolder()
+    hostsites[name] = result
+    site_policy = HostPolicySiteManager(result)
+    site_policy.__bases__ = (comps, secondary_comps)
+    # set the site manager and return
+    result.setSiteManager(site_policy)
+    return result
