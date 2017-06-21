b9b887ba57987878668486189c186c1c335dc603
Carlos Sanchez <papachoco@gmail.com>
2017-06-21 17:10:56 -0500

    mark site as created

--- src/nti/app/site/externalization.py
+++ src/nti/app/site/externalization.py
@@ -12,6 +12,9 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
+from nti.base.interfaces import ICreated
+from nti.base.interfaces import ICreatedTime
+
 from nti.externalization.interfaces import LocatedExternalDict
 from nti.externalization.interfaces import StandardExternalFields
 from nti.externalization.interfaces import IInternalObjectExternalizer
@@ -24,7 +27,9 @@ OID = StandardExternalFields.OID
 CLASS = StandardExternalFields.CLASS
 NTIID = StandardExternalFields.NTIID
 ITEMS = StandardExternalFields.ITEMS
+CREATOR = StandardExternalFields.CREATOR
 MIMETYPE = StandardExternalFields.MIMETYPE
+CREATED_TIME = StandardExternalFields.CREATED_TIME
 
 
 @component.adapter(IHostPolicyFolder)
@@ -40,4 +45,8 @@ class _SiteExternalizer(object):
         result['Name'] = self.site.__name__
         result[MIMETYPE] = 'application/vnd.nextthought.site'
         result[NTIID] = result[OID] = to_external_ntiid_oid(self.site)
+        if ICreated.providedBy(self.site):
+            result[CREATOR] = self.site.creator
+        if ICreatedTime.providedBy(self.site):
+            result[CREATED_TIME] = self.site.createdTime
         return result
