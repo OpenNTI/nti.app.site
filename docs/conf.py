018c00ad0fe3825cfb78186952a1f7b6c36bb089
Carlos Sanchez <papachoco@gmail.com>
2017-09-21 18:18:31 -0500

    whitespace

--- docs/conf.py
+++ docs/conf.py
@@ -108,7 +108,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ntiappsite doc'
+htmlhelp_basename = 'ntiappsitedoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -135,7 +135,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ntiappsite .tex', u'nti.app.site Documentation',
+    (master_doc, 'ntiappsite.tex', u'nti.app.site Documentation',
      u'NextThought', 'manual'),
 ]
 
@@ -145,7 +145,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'ntiappsite ', u'nti.app.site Documentation',
+    (master_doc, 'ntiappsite', u'nti.app.site Documentation',
      [author], 1)
 ]
 
@@ -156,8 +156,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ntiappsite ', u'nti.app.site Documentation',
-     author, 'ntiappsite ', 'One line description of project.',
+    (master_doc, 'ntiappsite', u'nti.app.site Documentation',
+     author, 'ntiappsite', 'One line description of project.',
      'Miscellaneous'),
 ]
 
