68aa0b8024fffd1b1e601e763d3e39391ddc3211
Josh Zuech <josh.zuech@nextthought.com>
2018-01-16 13:24:59 -0600

    SiteAdmin management views

--- src/nti/app/site/__init__.py
+++ src/nti/app/site/__init__.py
@@ -14,3 +14,5 @@ MessageFactory = zope.i18nmessageid.MessageFactory(__name__)
 SITE_ADMIN = "SiteAdmin"
 SITE_PROVIDER = 'PROVIDER'
 SITE_MIMETYPE = 'application/vnd.nextthought.site'
+
+VIEW_SITE_ADMINS = "SiteAdmins"
