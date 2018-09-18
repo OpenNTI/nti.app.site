39e323e0c8f95b90ba126f70aa745fa51958d1ed
Sheldon Smith <sheldon.smith@nextthought.com>
2018-09-18 16:33:50 -0500

    Missing add

--- src/nti/app/site/tests/test_site_zcml.py
+++ src/nti/app/site/tests/test_site_zcml.py
@@ -14,6 +14,7 @@ from hamcrest import none
 import nti.testing.base
 
 from zope import component
+from zope.configuration.exceptions import ConfigurationError
 
 from zope.dottedname import resolve as dottedname
 
@@ -52,10 +53,64 @@ ZCML_REGISTRATION = """
     </configure>
 </configure>"""
 
+DOUBLE_ZCML_REGISTRATION = """
+<configure  xmlns="http://namespaces.zope.org/zope"
+            xmlns:i18n="http://namespaces.zope.org/i18n"
+            xmlns:zcml="http://namespaces.zope.org/zcml"
+            xmlns:appsite="http://nextthought.com/ntp/appsite">
+
+    <include package="zope.component" file="meta.zcml" />
+    <include package="zope.security" file="meta.zcml" />
+    <include package="zope.component" />
+    <include package="." file="meta.zcml" />
+
+    <configure>
+        <appsite:createSites package="nti.app.sites.ifsta">
+            <appsite:createSite site_name="test_one_base"
+                                   base_names="nti.app.sites.ifsta.sites.IFSTA,"
+                                   parent_name="nti.app.sites.ifsta.sites.IFSTA">
+            </appsite:createSite>
+
+            <appsite:createSite site_name="test_one_base"
+                                   base_names="nti.app.sites.ifsta.sites.IFSTA_CHILD,"
+                                   parent_name="nti.app.sites.ifsta.sites.IFSTA_CHILD" />
+        </appsite:createSites>
+    </configure>
+</configure>"""
+
+MISSING_ZCML_REGISTRATION = """
+<configure  xmlns="http://namespaces.zope.org/zope"
+            xmlns:i18n="http://namespaces.zope.org/i18n"
+            xmlns:zcml="http://namespaces.zope.org/zcml"
+            xmlns:appsite="http://nextthought.com/ntp/appsite">
+
+    <include package="zope.component" file="meta.zcml" />
+    <include package="zope.security" file="meta.zcml" />
+    <include package="zope.component" />
+    <include package="." file="meta.zcml" />
+
+    <configure>
+        <appsite:createSites package="nti.app.sites.ifsta">
+            <appsite:createSite site_name="test_one_base"
+                                   base_names="missing,"
+                                   parent_name="missing">
+
+		        <utility factory=".policy.IFSTAFirehouseSitePolicyEventListener" />
+            </appsite:createSite>
+        </appsite:createSites>
+    </configure>
+</configure>"""
+
 
 class TestSiteZCMLRegistration(nti.testing.base.ConfiguringTestBase):
 
     def test_zcml_directive(self):
+        # Coverage on error cases
+        with self.assertRaises(ConfigurationError):
+            self.configure_string(DOUBLE_ZCML_REGISTRATION)
+        with self.assertRaises(ConfigurationError):
+            self.configure_string(MISSING_ZCML_REGISTRATION)
+
         self.configure_string(ZCML_REGISTRATION)
         test_one_base = component.getUtility(IComponents, name='test_one_base')
         ifsta_base = dottedname.resolve('nti.app.sites.ifsta.sites.IFSTA')
