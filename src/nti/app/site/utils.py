5f268a70e3bb8c4c13f70e302673e1ee55ef0996
Josh Zuech <josh.zuech@nextthought.com>
2020-06-28 22:43:33 -0500

    Only return current site admins as using admin seats

--- src/nti/app/site/utils.py
+++ src/nti/app/site/utils.py
@@ -8,12 +8,15 @@ from __future__ import division
 from zope import component
 from zope import interface
 
+from zope.component.hooks import getSite
+
 from nti.app.site.interfaces import ISiteMappingContainer
 from nti.app.site.interfaces import ISiteAdminSeatUserProvider
 
 from nti.app.site.model import SiteMappingContainer
 
 from nti.app.users.utils import get_site_admins
+from nti.app.users.utils import get_user_creation_site
 
 from nti.site.localutility import install_utility
 
@@ -35,8 +38,12 @@ def create_site_mapping_container():
 @interface.implementer(ISiteAdminSeatUserProvider)
 class _SiteAdminSeatUserProvider(object):
     """
-    Return site admin users
+    Return site admin users, only those created in our site.
     """
 
     def iter_users(self):
-        return get_site_admins()
+        site = getSite()
+        site_admins = get_site_admins()
+        for site_admin in site_admins:
+            if site == get_user_creation_site(site_admin):
+                yield site_admin
