89c2abacffe552f509e956821d5b2e55a6efdda1
Bobby Hagen <hagenrd@users.noreply.github.com>
2021-10-05 11:49:03 -0500

    Allow collections to be registered for site admin workspace. (#85)

--- src/nti/app/site/workspaces/interfaces.py
+++ src/nti/app/site/workspaces/interfaces.py
@@ -8,10 +8,18 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+from nti.appserver.workspaces import ICollection
+
 from nti.appserver.workspaces.interfaces import IWorkspace
 
 
 class ISiteAdminWorkspace(IWorkspace):
     """
     Workspace for a particular site admin
+    """
+
+
+class ISiteAdminCollection(ICollection):
+    """
+    Collection contained by the site admin workspace
     """
